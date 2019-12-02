21a449f561917215cca3e63de5e1fc59846cb6c0
Enderson <40265254+EndersonF@users.noreply.github.com>
2019-12-02 01:17:37 -0400

    Alteração - Normal

---
+++ src/app/tabNormal/tabNormal.page.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { ProviderService } from "../provider.service"
+
+@Component({
+  selector: 'app-tabNormal',
+  templateUrl: 'tabNormal.page.html',
+  styleUrls: ['tabNormal.page.scss']
+})
+export class TabNormalPage {
+
+  quests: any;
+  result: any;
+  constructor(public servidor:ProviderService) {
+    this.getQuestsFromProvider()
+  }
+
+  getQuestsFromProvider(){
+    this.servidor.getQuests("Normal")
+    .subscribe(
+      data => this.quests = data,
+      err => console.log(err)  
+    )
+  }
+  incrementNivel(param){
+    let body = {
+      action: 'increment',
+      quest: param,
+      xp:  1
+  };
+    this.servidor.postData(body,"ui")
+    .subscribe(
+      data => this.result = data,
+      err => console.log(err) 
+    )
+  }
+}
+
+
