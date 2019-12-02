eceff1e6be07f73571b55cb3c41809ac68e9ecb9
Enderson <40265254+EndersonF@users.noreply.github.com>
2019-12-02 01:12:55 -0400

    alteração - normal

---
+++ src/app/tab1.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ProviderService } from "../provider.service"
+
+import { HttpHeaders } from '@angular/common/http';
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  users: any;
+  icon = "http://www.gravatar.com/avatar.php?gravatar_id=";
+  
+  constructor(public servidor:ProviderService) {
+    this.getUserFromProvider()
+  }
+
+  getUserFromProvider(){
+    this.servidor.getUser()
+    .subscribe(
+      data => this.users = data,
+      err => console.log(err) 
+    )
+
+}
+  
+
+}
